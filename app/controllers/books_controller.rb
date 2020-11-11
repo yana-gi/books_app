a434f52638812a385d3d6f920434310f29ae8f1a
yana-gi <yanagisawa.hika@gmail.com>
2020-11-11 14:53:46 +0900

    並び順の指定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.page(params[:page])
+    @books = Book.order(:id).page(params[:page])
   end
 
   # GET /books/1
