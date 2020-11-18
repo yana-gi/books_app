1b82f007475edc1e47eb289a1789b6fd25225f91
yana-gi <yanagisawa.hika@gmail.com>
2020-11-18 17:22:10 +0900

    プロフィール編集後の画面遷移先変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   scope '(:locale)' do
-    resources :books
+    resources :users, :books
   end
 end
