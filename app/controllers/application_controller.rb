08ef6e8db9a5e3470bef9bdba4ea3d9e008f5620
yana-gi <yanagisawa.hika@gmail.com>
2020-11-18 17:10:56 +0900

    ログイン/ログアウト後の画面遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,25 @@ class ApplicationController < ActionController::Base
   # ログイン済ユーザーのみにアクセスを許可する
   before_action :authenticate_user!
 
+  # ロケールを変更する
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 
+  # デフォルトのロケール設定
   def default_url_options
     { locale: I18n.locale }
   end
+
+  # ログイン後は/booksに遷移する
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
+
+  # ログアウト後はログインページに遷移する
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
+
 end
