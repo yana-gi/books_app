b21c9db37763e8666fa2f3b19327c798f1c374aa
yana-gi <yanagisawa.hika@gmail.com>
2020-11-18 16:37:40 +0900

    サインアップとログインリンクの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
+  # ログイン済ユーザーのみにアクセスを許可する
+  before_action :authenticate_user!
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
