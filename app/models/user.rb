3b2af6b5074823a25290488fab267c4d05e52e18
yana-gi <yanagisawa.hika@gmail.com>
2020-11-19 14:42:11 +0900

    バリデーション見直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :name, length: { maximum: 30 }
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :name, presence: true
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }, if: :postal_code?
   validates :address, length: { maximum: 100 }
   validates :description, length: { maximum: 250 }
+
   has_many :books
 end
