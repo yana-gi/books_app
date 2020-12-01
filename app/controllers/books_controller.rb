8210d7e986bc07b6cdc0b2841052185f93daa944
yana-gi <yanagisawa.hika@gmail.com>
2020-12-01 13:51:54 +0900

    レビュー修正 N+1問題の解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.order(:id).page(params[:page])
+    @books = Book.includes(:user).order(:id).page(params[:page])
   end
 
   # GET /books/1
